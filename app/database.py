dc6259d941d897209f89736a4c3402c6cbeafc1a
elmotaz <elmalihi1@gmail.com>
2025-06-13 19:08:49 +0100

    v0.03

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://user:password@db:5432/prices"
+DATABASE_URL = "postgresql://user:password@https://ilnvgebjicli.eu-central-1.clawcloudrun.com/prices"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
